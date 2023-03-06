4524d074d0ccf91cbe4dfc5f9a60b2fdad060320
Ashish Nipane <ashishnipane09@gmail.com>
2023-03-06 13:04:30 +0530

    any

--- src/App.js
+++ src/App.js
@@ -1,55 +1,44 @@
-
 import logo from "./logo.svg";
-import "./App.css"
+import "./App.css";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import * as mdb from "mdb-ui-kit";
 import { Input } from "mdb-ui-kit";
 import ProductPage from "./ProductPage";
 
-
-import './App.css';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import Homecontent from './Homecontent';
-import Genre from './Genre';
-import BookDetails from './BookDetails';
-
-
-
+import "./App.css";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Homecontent from "./Homecontent";
+import Genre from "./Genre";
+import BookDetails from "./BookDetails";
+import CheckoutPage from "./CheckoutPage";
+import Checkout from "./Checkout";
 
 function App() {
   return (
-    <><>
+    <>
+      <>
+        <BrowserRouter>
+          <Navbar></Navbar>
 
-      <BrowserRouter>
-        <Navbar></Navbar>
-       
-
-        <Routes>
-          <Route path="/" element={<Homecontent/> }></Route>
-          <Route
-            path="/ProductPage"
-            element={<ProductPage></ProductPage>}
-          ></Route>
-          <Route
-            path="/Genre"
-            element={<Genre></Genre>}
-          ></Route>
-          <Route
-            path="/Details"
-            element={<BookDetails/>}
-          ></Route>
-        </Routes>
-        <Footer></Footer>
-        
-      </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homecontent />}></Route>
+            <Route
+              path="/ProductPage"
+              element={<ProductPage></ProductPage>}
+            ></Route>
+            <Route path="/Genre" element={<Genre></Genre>}></Route>
+            <Route path="/Details" element={<BookDetails />}></Route>
+            <Route
+              path="/CheckoutPage"
+              element={<CheckoutPage></CheckoutPage>}
+            ></Route>
+            <Route path="/Checkout" element={<Checkout></Checkout>}></Route>
+          </Routes>
+          <Footer></Footer>
+        </BrowserRouter>
+      </>
     </>
-
-   
-  
-   </>
-   
-
   );
 }
 
